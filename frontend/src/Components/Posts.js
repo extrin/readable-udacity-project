008e160b50c9212d7fd56801679366ea40501bb0
E.S <extrin13@gmail.com>
2017-12-07 08:26:17 +0000

    fix: Comment out unused imports

--- frontend/src/Components/Posts.js
+++ frontend/src/Components/Posts.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+//import { Link } from 'react-router-dom';
 
 const selectOptions = [
   { name: 'Vote score (asc.)', value: 'Vote score (asc.)' },
