f20ade616b3104a50d84f87ab2e33690d08ce67e
E.S <extrin13@gmail.com>
2017-12-13 23:04:33 +0300

    feat: Add api functions

--- frontend/src/Util/api.js
+++ frontend/src/Util/api.js
@@ -13,21 +13,157 @@ const baseUrl = 'http://127.0.0.1:3001';
 
 export function getCategories() {
   const endpoint = '/categories';
-  return fetch(baseUrl + endpoint, {
-    headers
-  }).then(res => res.json());
+  return fetch(baseUrl + endpoint, { headers }).then(res => res.json());
 }
 
 export function getPostsForCategory(category) {
   const endpoint = `/:${category}/posts`;
-  return fetch(baseUrl + endpoint, {
-    headers
-  }).then(res => res.json());
+  return fetch(baseUrl + endpoint, { headers }).then(res => res.json());
 }
 
 export function getPosts() {
   const endpoint = '/posts';
-  return fetch(baseUrl + endpoint, {
-    headers
-  }).then(res => res.json());
+  return fetch(baseUrl + endpoint, { headers }).then(res => res.json());
 }
+
+export function getPost(id) {
+  const endpoint = `/posts/:${id}`;
+  return fetch(baseUrl + endpoint, { headers }).then(res => res.json());
+}
+
+export function createPost(id, timestamp, title, body, author, category) {
+  const endpoint = '/posts';
+  return fetch(baseUrl + endpoint, {
+    method: 'post',
+    headers: headers,
+    body: `id=${id}&timestamp=${timestamp}&title=${title}&body=${body}&author=${author}&category=${category}`
+  })
+    .then(json)
+    .then(function(data) {
+      console.log('Request succeeded with JSON response', data);
+    })
+    .catch(function(error) {
+      console.log('Request failed', error);
+    });
+}
+
+export function voteOnPost(id, option) {
+  const endpoint = `/posts/:${id}`;
+  return fetch(baseUrl + endpoint, {
+    method: 'post',
+    headers: headers,
+    body: `option=${option}`
+  })
+    .then(json)
+    .then(function(data) {
+      console.log('Request succeeded with JSON response', data);
+    })
+    .catch(function(error) {
+      console.log('Request failed', error);
+    });
+}
+
+export function updatePost(id, title, body) {
+  const endpoint = `/posts/:${id}`;
+  return fetch(baseUrl + endpoint, {
+    method: 'put',
+    headers: headers,
+    body: `title=${title}&body=${body}`
+  })
+    .then(json)
+    .then(function(data) {
+      console.log('Request succeeded with JSON response', data);
+    })
+    .catch(function(error) {
+      console.log('Request failed', error);
+    });
+}
+
+export function deletePost(id) {
+  const endpoint = `/posts/:${id}`;
+  return fetch(baseUrl + endpoint, {
+    method: 'delete',
+    headers: headers
+  })
+    .then(json)
+    .then(function(data) {
+      console.log('Request succeeded with JSON response', data);
+    })
+    .catch(function(error) {
+      console.log('Request failed', error);
+    });
+}
+
+export function getComments(postId) {
+  const endpoint = `/posts/:${id}/comments`;
+  return fetch(baseUrl + endpoint, { headers }).then(res => res.json());
+}
+
+export function getComment(id) {
+  const endpoint = `/comments/:${id}`;
+  return fetch(baseUrl + endpoint, { headers }).then(res => res.json());
+}
+
+export function createComment(postId, id, timestamp, author, body) {
+  const endpoint = '/comments';
+  return fetch(baseUrl + endpoint, {
+    method: 'post',
+    headers: headers,
+    body: `id=${id}&timestamp=${timestamp}&body=${body}&author=${author}&parentId=${postId}`
+  })
+    .then(json)
+    .then(function(data) {
+      console.log('Request succeeded with JSON response', data);
+    })
+    .catch(function(error) {
+      console.log('Request failed', error);
+    });
+}
+
+export function voteOnComment(id, option) {
+  const endpoint = `/comments/:${id}`;
+  return fetch(baseUrl + endpoint, {
+    method: 'post',
+    headers: headers,
+    body: `option=${option}`
+  })
+    .then(json)
+    .then(function(data) {
+      console.log('Request succeeded with JSON response', data);
+    })
+    .catch(function(error) {
+      console.log('Request failed', error);
+    });
+}
+
+export function updateComment(id, timestamp, body) {
+  const endpoint = `/comments/:${id}`;
+  return fetch(baseUrl + endpoint, {
+    method: 'put',
+    headers: headers,
+    body: `timestamp=${timestamp}&body=${body}`
+  })
+    .then(json)
+    .then(function(data) {
+      console.log('Request succeeded with JSON response', data);
+    })
+    .catch(function(error) {
+      console.log('Request failed', error);
+    });
+}
+
+export function deleteComment(id) {
+  const endpoint = `/comments/:${id}`;
+  return fetch(baseUrl + endpoint, {
+    method: 'delete',
+    headers: headers
+  })
+    .then(json)
+    .then(function(data) {
+      console.log('Request succeeded with JSON response', data);
+    })
+    .catch(function(error) {
+      console.log('Request failed', error);
+    });
+}
+
