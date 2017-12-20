aab7cf38af440934c9fcc7a235ae0ab86b2a927e
E.S <extrin13@gmail.com>
2017-12-21 00:22:49 +0300

    feat: Connect comment edit component to the store

--- frontend/src/Components/CommentEdit.js
+++ frontend/src/Components/CommentEdit.js
@@ -1,14 +1,50 @@
-import React from 'react'
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { editComment } from '../Actions/Comment';
 
-function CommentEdit(comment) {
-  return (
-    <div className='comment-edit'>
-    	<p>Edit comment</p>
-    	<p>Author: {comment.author}</p>
-    	<input className='comment-body-input' type='text' value={comment.body} required/>
-    	<button className='comment-save-btn'>Save</button>
-    </div>
-  );
+class CommentEdit extends Component {
+  state = { body: '' };
+
+  updateBody = commentBody => {
+    this.setState({ body: commentBody });
+  };
+
+  render() {
+    return (
+      <div className="comment-edit">
+        <p>Edit comment</p>
+        <p>Author: {this.props.author}</p>
+        <input
+          className="comment-body-input"
+          type="text"
+          value={this.state.body || this.props.body}
+          onChange={event => this.updateBody(event.target.value)}
+          required
+        />
+        <button
+          className="comment-save-btn"
+          onClick={() => this.props.saveComment(this.props.id, this.state.body)}
+        >
+          Save
+        </button>
+      </div>
+    );
+  }
 }
 
-export default CommentEdit;
+const mapStateToProps = state => {
+  const comment = state.comments.filter(
+    com => com.id === state.selections.selectedComment
+  );
+  return {
+    author: comment.author,
+    body: comment.body,
+    id: state.selections.selectedComment
+  };
+};
+
+const mapDispatchToProps = dispatch => ({
+  saveComment: (id, body) => dispatch(editComment(id, body))
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(CommentEdit);
