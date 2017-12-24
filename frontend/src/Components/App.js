ec5a89853fa89d0fc7ba684458da5b2a62857c91
E.S <extrin13@gmail.com>
2017-12-25 00:12:49 +0300

    feat: Use custom Appbar

--- frontend/src/Components/App.js
+++ frontend/src/Components/App.js
@@ -1,9 +1,8 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import AppBar from 'material-ui/AppBar';
-import Home from 'material-ui/svg-icons/action/home';
-//import '../App.css';
+import '../App.css';
+import CustomAppBar from './CustomAppBar';
 import MainView from './MainView';
 import PostCreate from './PostCreate';
 import PostView from './PostView';
@@ -12,27 +11,26 @@ import PostEdit from './PostEdit';
 class App extends Component {
   render() {
     return (
-      <Router>
-        <MuiThemeProvider>
+      <MuiThemeProvider>
+        <Router>
           <div className="app">
-            <AppBar
-              title={'Readable'}
-              iconElementLeft={
-                <Link to="/">
-                  <Home />
-                </Link>
-              }
-            />
-            <Route exact path="/" component={MainView} />
-            <Switch>
-              <Route exact path="/create" component={PostCreate} />
-              <Route exact path="/:category" component={MainView} />
-            </Switch>
-            <Route exact path="/:category/:post_id" component={PostView} />
-            <Route exact path="/:category/:post_id/edit" component={PostEdit} />
+            <CustomAppBar />
+            <div className="app-body">
+              <Route exact path="/" component={MainView} />
+              <Switch>
+                <Route exact path="/create" component={PostCreate} />
+                <Route exact path="/:category" component={MainView} />
+              </Switch>
+              <Route exact path="/:category/:post_id" component={PostView} />
+              <Route
+                exact
+                path="/:category/:post_id/edit"
+                component={PostEdit}
+              />
+            </div>
           </div>
-        </MuiThemeProvider>
-      </Router>
+        </Router>
+      </MuiThemeProvider>
     );
   }
 }
--- frontend/src/Components/MainView.js
+++ frontend/src/Components/MainView.js
@@ -1,8 +1,6 @@
 import React from 'react';
-import Categories from './Categories';
 import Posts from './Posts';
 import ErrorBoundary from './ErrorBoundary';
-import { Link } from 'react-router-dom';
 import FloatingActionButton from 'material-ui/FloatingActionButton';
 import ContentAdd from 'material-ui/svg-icons/content/add';
 
@@ -10,7 +8,6 @@ function MainView(props) {
   return (
     <div className="default-view">
       <ErrorBoundary>
-        <Categories />
         <Posts />
       </ErrorBoundary>
       <FloatingActionButton className="create-post" href="/create">
