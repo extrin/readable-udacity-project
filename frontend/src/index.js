89db8ffe23fedfc4932fbc91f3c50add4a88a40b
E.S <extrin13@gmail.com>
2017-12-30 01:12:42 +0300

    feat: Style modals and set app element for them

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ReactModal from 'react-modal';
 import './index.css';
 import App from './Components/App';
 import registerServiceWorker from './registerServiceWorker';
@@ -7,6 +8,8 @@ import { Provider } from 'react-redux';
 import store from './Store/Store';
 import { getPosts } from './Actions/Post';
 import { getCategories } from './Actions/Category';
+
+ReactModal.setAppElement('#root');
 
 ReactDOM.render(
   <Provider store={store}>
