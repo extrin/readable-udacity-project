5cbcc74d76104fbdb7f1ba3b0ee372902eb82def
E.S <extrin13@gmail.com>
2017-12-29 16:28:10 +0300

    feat: Make appbar title a link

--- frontend/src/Components/CustomAppBar.js
+++ frontend/src/Components/CustomAppBar.js
@@ -2,6 +2,9 @@ import React from 'react';
 import AppBar from 'material-ui/AppBar';
 import Drawer from 'material-ui/Drawer';
 import Categories from './Categories';
+import { connect } from 'react-redux';
+import { selectCategory } from '../Actions/Category';
+import { Link } from 'react-router-dom';
 
 class CustomAppBar extends React.Component {
   state = { drawerOpen: false };
@@ -12,7 +15,12 @@ class CustomAppBar extends React.Component {
     return (
       <div className="custom-app-bar">
         <AppBar
-          title="Readable"
+          title={
+            <Link style={{ 'text-decoration': 'none', color: 'white' }} to="/">
+              Readable
+            </Link>
+          }
+          onTitleClick={() => this.props.openCategory()}
           showMenuIconButton={true}
           onLeftIconButtonClick={() => this.handleDrawer()}
           style={{ position: 'fixed' }}
@@ -29,4 +37,8 @@ class CustomAppBar extends React.Component {
   }
 }
 
-export default CustomAppBar;
+const mapDispatchToProps = dispatch => ({
+  openCategory: category => dispatch(selectCategory(category))
+});
+
+export default connect(null, mapDispatchToProps)(CustomAppBar);
