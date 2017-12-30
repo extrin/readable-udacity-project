9b8c6dc2d03eb4660beaa3be46ecc7af1e32b52a
E.S <extrin13@gmail.com>
2017-12-30 20:30:25 +0300

    feat: Give material ui to comment creating component

--- frontend/src/Components/CommentCreate.js
+++ frontend/src/Components/CommentCreate.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { createComment } from '../Actions/Comment';
+import TextField from 'material-ui/TextField';
+import RaisedButton from 'material-ui/RaisedButton';
+import { closeCommentCreateModal } from '../Actions/Modal';
 
 class CommentCreate extends Component {
   state = {
@@ -18,32 +21,41 @@ class CommentCreate extends Component {
 
   render() {
     const { commentAuthor, commentBody } = this.state;
-    const { addComment, parentId } = this.props;
+    const { addComment, parentId, closeModal } = this.props;
 
     return (
       <div className="comment-create">
-        <input
+        <TextField
           className="comment-author-input"
           value={commentAuthor}
           onChange={event => this.updateAuthor(event.target.value)}
-          type="text"
-          placeholder="Comment Author"
+          floatingLabelText="Enter your nickname"
+          hintText="Author name"
+          errorText={this.value === '' && 'This field is required'}
           required
         />
-        <input
+        <TextField
           className="comment-body-input"
           value={commentBody}
           onChange={event => this.updateBody(event.target.value)}
-          type="text"
-          placeholder="Comment Body"
+          multiLine={true}
+          rows={5}
+          floatingLabelText="Enter your comment"
+          hintText="Comment body"
+          errorText={this.value === '' && 'This field is required'}
           required
         />
-        <button
+        <RaisedButton
+          className="cancel-btn"
+          onClick={() => closeModal()}
+          label="CANCEL"
+        />
+        <RaisedButton
           className="comment-save-btn"
           onClick={() => addComment(commentBody, commentAuthor, parentId)}
-        >
-          Save
-        </button>
+          label="SAVE"
+          secondary={true}
+        />
       </div>
     );
   }
@@ -54,8 +66,11 @@ const mapStateToProps = state => ({
 });
 
 const mapDispatchToProps = dispatch => ({
-  addComment: (body, author, parentId) =>
-    dispatch(createComment(body, author, parentId))
+  addComment: (body, author, parentId) => {
+    dispatch(createComment(body, author, parentId));
+    dispatch(closeCommentCreateModal());
+  },
+  closeModal: () => dispatch(closeCommentCreateModal())
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(CommentCreate);
