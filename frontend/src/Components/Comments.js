c6f90b0a1d3cb5ed5d7fddab17505a8b2afbc51c
E.S <extrin13@gmail.com>
2018-01-01 23:30:38 +0300

    feat: Use CustomCard component

--- frontend/src/Components/Comments.js
+++ frontend/src/Components/Comments.js
@@ -1,71 +1,22 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Modal from 'react-modal';
 import sortBy from 'sort-by';
-import { Card, CardHeader, CardActions, CardText } from 'material-ui/Card';
-import FlatButton from 'material-ui/FlatButton';
-import Remove from 'material-ui/svg-icons/content/clear';
-import Edit from 'material-ui/svg-icons/image/edit';
-import ArrowUp from 'material-ui/svg-icons/navigation/arrow-upward';
-import ArrowDown from 'material-ui/svg-icons/navigation/arrow-downward';
 import { connect } from 'react-redux';
-import {
-  deleteComment,
-  changeCommentVotescore,
-  selectComment
-} from '../Actions/Comment';
-import { openCommentEditModal, closeCommentEditModal } from '../Actions/Modal';
 import CommentEdit from './CommentEdit';
-import { getStringDate } from '../Util/Helpers';
+import CustomCard from './CustomCard';
+import { closeCommentEditModal } from '../Actions/Modal';
 
-class Comments extends Component {
+class Comments extends React.Component {
   render() {
-    const {
-      comments,
-      upVote,
-      downVote,
-      deleteComment,
-      openForEdit,
-      closeModal
-    } = this.props;
-    const editModalOpen = this.props.modalMode === 'opened' ? true : false;
+    const { comments, closeModal, modalMode } = this.props;
+    const editModalOpen = modalMode === 'opened' ? true : false;
     return (
       <div className="comments-list">
-        {comments.sort(sortBy('-timestamp')).map(comment => (
-          <Card className="comment" key={comment.id}>
-            <CardActions>
-              <FlatButton
-                className="vote-up"
-                onClick={() => upVote(comment.id)}
-                icon={<ArrowUp color="red" />}
-              />
-              <FlatButton
-                className="voteScore"
-                disabled={true}
-                label={comment.voteScore}
-              />
-              <FlatButton
-                className="vote-down"
-                onClick={() => downVote(comment.id)}
-                icon={<ArrowDown color="blue" />}
-              />
-              <FlatButton
-                className="comment-edit-btn"
-                onClick={() => openForEdit(comment.id)}
-                icon={<Edit />}
-              />
-              <FlatButton
-                className="comment-delete-btn"
-                onClick={() => deleteComment(comment.id)}
-                icon={<Remove />}
-              />
-            </CardActions>
-            <CardHeader
-              title={comment.author}
-              subtitle={getStringDate(comment.timestamp)}
-            />
-            <CardText className="comment-body">{comment.body}</CardText>
-          </Card>
-        ))}
+        {comments
+          .sort(sortBy('-timestamp'))
+          .map(comment => (
+            <CustomCard mode="comment" id={comment.id} key={comment.id} />
+          ))}
         <Modal
           className="modal"
           overlayClassName="overlay"
@@ -86,14 +37,9 @@ const mapStateToProps = state => ({
 });
 
 const mapDispatchToProps = dispatch => ({
-  upVote: id => dispatch(changeCommentVotescore(id, 'upVote')),
-  downVote: id => dispatch(changeCommentVotescore(id, 'downVote')),
-  removeComment: id => dispatch(deleteComment(id)),
-  openForEdit: id => {
-    dispatch(selectComment(id));
-    dispatch(openCommentEditModal());
-  },
-  closeModal: () => dispatch(closeCommentEditModal())
+  closeModal: () => {
+    dispatch(closeCommentEditModal());
+  }
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(Comments);
--- frontend/src/Components/PostView.js
+++ frontend/src/Components/PostView.js
@@ -1,96 +1,23 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import React from 'react';
 import { connect } from 'react-redux';
 import Modal from 'react-modal';
-import IconButton from 'material-ui/IconButton';
 import FlatButton from 'material-ui/FlatButton';
-import Paper from 'material-ui/Paper';
-import Star from 'material-ui/svg-icons/toggle/star';
-import ArrowUp from 'material-ui/svg-icons/navigation/arrow-upward';
-import ArrowDown from 'material-ui/svg-icons/navigation/arrow-downward';
 import Comment from 'material-ui/svg-icons/communication/comment';
-import Remove from 'material-ui/svg-icons/content/clear';
-import Edit from 'material-ui/svg-icons/image/edit';
-import Subheader from 'material-ui/Subheader';
-import { deletePost, changePostVotescore, selectPost } from '../Actions/Post';
 import {
   openCommentCreateModal,
   closeCommentCreateModal
 } from '../Actions/Modal';
 import CommentCreate from './CommentCreate';
 import Comments from './Comments';
-import { getStringDate } from '../Util/Helpers';
+import CustomCard from './CustomCard';
 
-class PostView extends Component {
+class PostView extends React.Component {
   render() {
     const commentModalOpen = this.props.modalMode === 'opened' ? true : false;
-    const {
-      post,
-      upVote,
-      downVote,
-      removePost,
-      openPost,
-      openCommentModal,
-      closeCommentModal
-    } = this.props;
+    const { postId, openCommentModal, closeCommentModal } = this.props;
     return (
       <div className="post-view">
-        <Paper zDepth={2} className="post-details">
-          <Paper zDepth={1} className="post-actions">
-            <div className="post-actions-voting">
-              <IconButton
-                className="vote-up"
-                onClick={() => upVote(post.id)}
-                tooltip="Vote Up"
-              >
-                <ArrowUp />
-              </IconButton>
-              <FlatButton
-                disabled
-                tooltip="Votescore"
-                label={post.voteScore}
-                labelPosition="after"
-                icon={<Star color="pink" />}
-              />
-
-              <IconButton
-                className="vote-down"
-                onClick={() => downVote(post.id)}
-                tooltip="Vote Down"
-              >
-                <ArrowDown />
-              </IconButton>
-            </div>
-            <div className="post-actions-managing">
-              <IconButton
-                className="post-edit-btn"
-                containerElement={
-                  <Link
-                    className="post-edit-link"
-                    to={`/${post.category}/${post.id}/edit`}
-                  />
-                }
-                onClick={() => openPost(post.id)}
-              >
-                <Edit />
-              </IconButton>
-              <IconButton
-                className="post-delete-btn"
-                onClick={() => removePost(post.id)}
-              >
-                <Remove />
-              </IconButton>
-            </div>
-          </Paper>
-          <h2 className="post-title">{post.title}</h2>
-          <Subheader className="post-author">
-            {getStringDate(post.timestamp)} by {post.author}
-          </Subheader>
-
-          <div className="post-body">
-            <p>{post.body}</p>
-          </div>
-        </Paper>
+        <CustomCard mode="post" id={postId} />
         <FlatButton
           className="add-comment-btn"
           onClick={() => openCommentModal()}
@@ -98,7 +25,6 @@ class PostView extends Component {
           labelPosition="before"
           icon={<Comment />}
         />
-
         <Modal
           className="modal"
           overlayClassName="overlay"
@@ -108,7 +34,6 @@ class PostView extends Component {
         >
           <CommentCreate />
         </Modal>
-
         <div className="post-comments">
           <Comments />
         </div>
@@ -118,15 +43,11 @@ class PostView extends Component {
 }
 
 const mapStateToProps = state => ({
-  post: state.posts.find(post => post.id === state.selections.selectedPost),
+  postId: state.selections.selectedPost,
   modalMode: state.modals.commentCreateModal
 });
 
 const mapDispatchToProps = dispatch => ({
-  upVote: id => dispatch(changePostVotescore(id, 'upVote')),
-  downVote: id => dispatch(changePostVotescore(id, 'downVote')),
-  removePost: id => dispatch(deletePost(id)),
-  openPost: id => dispatch(selectPost(id)),
   openCommentModal: () => dispatch(openCommentCreateModal()),
   closeCommentModal: () => dispatch(closeCommentCreateModal())
 });
