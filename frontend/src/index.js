cf509d8f637ad6020bb01994f9a8c396887212dd
E.S <extrin13@gmail.com>
2018-01-02 17:35:51 +0300

    fix: Load data before rendering

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom';
 import ReactModal from 'react-modal';
 import './index.css';
 import App from './Components/App';
-import registerServiceWorker from './registerServiceWorker';
 import { Provider } from 'react-redux';
 import store from './Store/Store';
 import { getPosts } from './Actions/Post';
 import { getCategories } from './Actions/Category';
+
+getCategories()(store.dispatch);
+getPosts()(store.dispatch);
 
 ReactModal.setAppElement('#root');
 
@@ -17,6 +19,3 @@ ReactDOM.render(
   </Provider>,
   document.getElementById('root')
 );
-getCategories()(store.dispatch);
-getPosts()(store.dispatch);
-registerServiceWorker();
