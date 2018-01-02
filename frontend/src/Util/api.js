47e497ae280d59bd84f27dbaddc72b1826d17dbc
E.S <extrin13@gmail.com>
2018-01-02 17:36:31 +0300

    fix: Use not random token

--- frontend/src/Util/api.js
+++ frontend/src/Util/api.js
@@ -1,9 +1,9 @@
 // Generate a unique token for storing your bookshelf data on the backend server.
-let token = localStorage.token;
-if (!token)
-  token = localStorage.token = Math.random()
-    .toString(36)
-    .substr(-8);
+let token = '545nm56r'; //localStorage.token;
+// if (!token)
+//   token = localStorage.token = Math.random()
+//     .toString(36)
+//     .substr(-8);
 
 const headers = { Authorization: token };
 const baseUrl = 'http://127.0.0.1:3001';
