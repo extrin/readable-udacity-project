9469c77c92d90527322e72f697c472e8513d340c
E.S <extrin13@gmail.com>
2018-01-08 01:06:12 +0300

    refactor: Move initial data loading to store

--- frontend/src/Store/Store.js
+++ frontend/src/Store/Store.js
@@ -2,6 +2,8 @@ import { createStore, applyMiddleware, compose } from 'redux';
 import ReduxThunk from 'redux-thunk';
 import logger from 'redux-logger';
 import rootReducer from '../Reducers';
+import { getPosts } from '../Actions/Post';
+import { getCategories } from '../Actions/Category';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
@@ -9,5 +11,8 @@ const store = createStore(
   rootReducer,
   composeEnhancers(applyMiddleware(ReduxThunk, logger))
 );
+
+getCategories()(store.dispatch);
+getPosts()(store.dispatch);
 
 export default store;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,11 +5,6 @@ import './index.css';
 import App from './Components/App';
 import { Provider } from 'react-redux';
 import store from './Store/Store';
-import { getPosts } from './Actions/Post';
-import { getCategories } from './Actions/Category';
-
-getCategories()(store.dispatch);
-getPosts()(store.dispatch);
 
 ReactModal.setAppElement('#root');
 
