cd912d8507170eb9bd7ed1d6127a3e169e87b1c2
E.S <extrin13@gmail.com>
2018-01-18 00:07:48 +0300

    feat: Show spinner while data is loading

--- frontend/src/Components/Routes.js
+++ frontend/src/Components/Routes.js
@@ -1,27 +1,33 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
+import { connect } from 'react-redux';
 import Posts from './Posts';
 import PostCreate from './PostCreate';
 import PostView from './PostView';
 import PostEdit from './PostEdit';
-//import Loading from 'react-loading';
-// const LoadingSpinner = () => {
-//   return <Loading delay={200} type="spin" color="#222" className="loading" />;
-// };
+import LoadingSpinner from './LoadingSpinner';
 
 class Routes extends React.Component {
   render() {
     return (
       <div className="app-body">
-        <Switch>
-          <Route exact path="/create" component={PostCreate} />
-          <Route exact path="/:category?" component={Posts} />
-        </Switch>
-        <Route exact path="/:category/:post_id" component={PostView} />
-        <Route path="/:category/:post_id/edit" component={PostEdit} />
+        {this.props.postsLoaded === true ? (
+          <div>
+            <Switch>
+              <Route exact path="/create" component={PostCreate} />
+              <Route exact path="/:category?" component={Posts} />
+            </Switch>
+            <Route exact path="/:category/:post_id" component={PostView} />
+            <Route path="/:category/:post_id/edit" component={PostEdit} />
+          </div>
+        ) : (
+          <LoadingSpinner />
+        )}
       </div>
     );
   }
 }
 
-export default Routes;
+const mapStateToProps = state => ({ postsLoaded: state.loading.postsLoaded });
+
+export default connect(mapStateToProps)(Routes);
