077a0f0008a74929c5f8b5eb19c758dd0887510d
E.S <extrin13@gmail.com>
2018-01-21 02:17:58 +0300

    feat: Add array of post ids to store

--- frontend/src/Reducers/index.js
+++ frontend/src/Reducers/index.js
@@ -4,10 +4,12 @@ import comments from './Comments';
 import loading from './Loading';
 import modals from './Modals';
 import posts from './Posts';
+import postsIds from './PostsIds';
 import selections from './Selections';
 
 export default combineReducers({
   posts,
+  postsIds,
   comments,
   categories,
   selections,
